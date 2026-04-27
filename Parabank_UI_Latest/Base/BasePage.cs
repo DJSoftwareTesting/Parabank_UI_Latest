9a94ff5edb587d4728bfc2132765d289fc183a5c
dipak5229 <dipak.jagadale5229@gmail.com>
2026-04-27 12:12:23 +0530

    Resolved PR comments

--- Parabank_UI_Latest/Base/BasePage.cs
+++ Parabank_UI_Latest/Base/BasePage.cs
@@ -1,7 +1,4 @@
 ﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Parabank_UI_Latest.Base
 {
--- Parabank_UI_Latest/Tests/LoginTests.cs
+++ Parabank_UI_Latest/Tests/LoginTests.cs
@@ -14,7 +14,6 @@ namespace Parabank_UI_Latest.Tests
             loginpage = new LoginPage(driver);
             loginpage.navigateToBankUrl();
             loginpage.Login(ConfigManager.Get("username"), ConfigManager.Get("password"));
-            //Assert.That(driver.Title, Is.EqualTo("ParaBank | Accounts Overview"), "Title is not matching.");
             Assert.That(driver.Title, Is.EqualTo("ParaBank | Error"), "Title is not matching.");
         }
     }
